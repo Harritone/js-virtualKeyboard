1d510c92ce2889667f50ca07d43b85ece37e11e6
Harritone <yourrasik@gmail.com>
2020-12-19 19:23:19 +1000

    Initial commit

---
+++ js/layouts/ru.js
@@ -0,0 +1,332 @@
+export default [
+  {
+    small: 'Capslock',
+    shift: null,
+    code: 'Capslock',
+  },
+  {
+    small: 'ё',
+    shift: 'Ё',
+    code: 'Backqoute',
+  },
+  {
+    small: '1',
+    shift: '!',
+    code: 'Digit1',
+  },
+  {
+    small: '2',
+    shift: '"',
+    code: 'Digit2',
+  },
+  {
+    small: '3',
+    shift: '№',
+    code: 'Digit3',
+  },
+  {
+    small: '4',
+    shift: '$',
+    code: 'Digit4',
+  },
+  {
+    small: '5',
+    shift: '%',
+    code: 'Digit5',
+  },
+  {
+    small: '6',
+    shift: '^',
+    code: 'Digit6',
+  },
+  {
+    small: '7',
+    shift: '&',
+    code: 'Digit7',
+  },
+  {
+    small: '8',
+    shift: '*',
+    code: 'Digit8',
+  },
+  {
+    small: '9',
+    shift: '(',
+    code: 'Digit9',
+  },
+  {
+    small: '0',
+    shift: ')',
+    code: 'Digit0',
+  },
+  {
+    small: '-',
+    shift: '_',
+    code: 'Minus',
+  },
+  {
+    small: '=',
+    shift: '+',
+    code: 'Equal',
+  },
+  {
+    small: 'Backspace',
+    shift: null,
+    code: 'Backspace',
+  },
+  {
+    small: 'Tab',
+    shift: null,
+    code: 'Tab',
+  },
+  {
+    small: 'й',
+    shift: 'Й',
+    code: 'KeyQ',
+  },
+  {
+    small: 'ц',
+    shift: 'Ц',
+    code: 'KeyW',
+  },
+  {
+    small: 'у',
+    shift: 'У',
+    code: 'KeyE',
+  },
+  {
+    small: 'к',
+    shift: 'К',
+    code: 'KeyR',
+  },
+  {
+    small: 'е',
+    shift: 'Е',
+    code: 'KeyT',
+  },
+  {
+    small: 'н',
+    shift: 'Н',
+    code: 'KeyY',
+  },
+  {
+    small: 'г',
+    shift: 'Г',
+    code: 'KeyU',
+  },
+  {
+    small: 'ш',
+    shift: 'Ш',
+    code: 'KeyI',
+  },
+  {
+    small: 'щ',
+    shift: 'Щ',
+    code: 'KeyO',
+  },
+  {
+    small: 'з',
+    shift: 'З',
+    code: 'KeyP',
+  },
+  {
+    small: 'х',
+    shift: 'Х',
+    code: 'Bracket Left',
+  },
+  {
+    small: 'ъ',
+    shift: 'Ъ',
+    code: 'Bracket Right',
+  },
+  {
+    small: '\\',
+    shift: '/',
+    code: 'Backslash',
+  },
+  {
+    small: 'ф',
+    shift: 'Ф',
+    code: 'KeyA',
+  },
+  {
+    small: 'ы',
+    shift: 'Ы',
+    code: 'KeyS',
+  },
+  {
+    small: 'в',
+    shift: 'В',
+    code: 'KeyD',
+  },
+  {
+    small: 'а',
+    shift: 'А',
+    code: 'KeyF',
+  },
+  {
+    small: 'п',
+    shift: 'П',
+    code: 'KeyG',
+  },
+  {
+    small: 'р',
+    shift: 'Р',
+    code: 'KeyH',
+  },
+  {
+    small: 'о',
+    shift: 'О',
+    code: 'KeyJ',
+  },
+  {
+    small: 'л',
+    shift: 'Л',
+    code: 'KeyK',
+  },
+  {
+    small: 'д',
+    shift: 'Д',
+    code: 'KeyL',
+  },
+  {
+    small: 'ж',
+    shift: 'Ж',
+    code: 'Semicolon',
+  },
+  {
+    small: 'э',
+    shift: 'Э',
+    code: 'Qoute',
+  },
+  {
+    small: 'Enter',
+    shift: null,
+    code: 'Enter',
+  },
+  {
+    small: 'ShiftLeft',
+    shift: null,
+    code: 'ShiftLeft',
+  },
+  {
+    small: '\\',
+    shift: '\\',
+    code: 'IntlBackslash',
+  },
+  {
+    small: 'я',
+    shift: 'Я',
+    code: 'KeyZ',
+  },
+  {
+    small: 'ч',
+    shift: 'Ч',
+    code: 'KeyX',
+  },
+  {
+    small: 'с',
+    shift: 'С',
+    code: 'KeyC',
+  },
+  {
+    small: 'м',
+    shift: 'М',
+    code: 'KeyV',
+  },
+  {
+    small: 'и',
+    shift: 'И',
+    code: 'KeyB',
+  },
+  {
+    small: 'т',
+    shift: 'Т',
+    code: 'KeyN',
+  },
+  {
+    small: 'ь',
+    shift: 'Ь',
+    code: 'KeyM',
+  },
+  {
+    small: 'б',
+    shift: 'Б',
+    code: 'Comma',
+  },
+  {
+    small: 'ю',
+    shift: 'Ю',
+    code: 'Period',
+  },
+  {
+    small: '.',
+    shift: ',',
+    code: 'Slash',
+  },
+  {
+    small: '/',
+    shift: '/',
+    code: 'IntlRo',
+  },
+  {
+    small: 'ArrowUp',
+    shift: null,
+    code: 'ArrowUp',
+  },
+  {
+    small: 'ShiftRIght',
+    shift: null,
+    code: 'ShiftRight',
+  },
+  {
+    small: 'Control',
+    shift: null,
+    code: 'ControlLeft',
+  },
+  {
+    small: 'Meta',
+    shift: null,
+    code: 'MetaLeft',
+  },
+  {
+    small: 'AltLeft',
+    shift: null,
+    code: 'AltLeft',
+  },
+  {
+    small: 'Space',
+    shift: null,
+    code: 'Space',
+  },
+  {
+    small: 'AltRight',
+    shift: null,
+    code: 'AltRight',
+  },
+  {
+    small: 'MetaRight',
+    shift: null,
+    code: 'MetaRight',
+  },
+  {
+    small: 'ControlRight',
+    shift: null,
+    code: 'ControlRight',
+  },
+  {
+    small: 'ArrowLeft',
+    shift: null,
+    code: 'ArrowLeft',
+  },
+  {
+    small: 'ArrowDown',
+    shift: null,
+    code: 'ArrowDown',
+  },
+  {
+    small: 'ArrowRight',
+    shift: null,
+    code: 'ArrowRight',
+  },
+];
---
+++ js/utils/create.js
@@ -0,0 +1,7 @@
+/**
+ * @param {HTMLElement} el
+ * @param {String} className
+ * @param {HTMLElement} child
+ * @param {HTMLElement} parent
+ * @param {...array} dataAttr
+ */
